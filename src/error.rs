3604580936d101241461df6ac5e01ca713087ad1
panicbit <panicbit@users.noreply.github.com>
2015-03-05 05:52:51 +0100

    Enhance RecaptchaError

--- src/error.rs
+++ src/error.rs
@@ -8,15 +8,19 @@ use super::RecaptchaErrorCode;
 
 pub enum RecaptchaError {
     Codes(HashSet<RecaptchaErrorCode>),
-    Wrapped(Box<Error>)
+    Decoder(DecoderError),
+    Http(HttpError),
+    Io(io::Error),
 }
 
 impl Display for RecaptchaError {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         use self::RecaptchaError::*;
-        match *self {
-            Codes(ref errs) => write!(f, "{} ({:?})", self.description(), errs),
-            Wrapped(ref err) => write!(f, "{} ({})", self.description(), err)
+        match self {
+            &Codes(ref errs) => write!(f, "{} ({:?})", self.description(), errs),
+            &Decoder(ref e) => Display::fmt(e, f),
+            &Http(ref e) => Display::fmt(e, f),
+            &Io(ref e) => Display::fmt(e, f)
         }
     }
 }
@@ -34,27 +38,29 @@ impl Error for RecaptchaError {
 
     fn cause(&self) -> Option<&Error> {
         use self::RecaptchaError::*;
-        match *self {
-            Codes(_) => None,
-            Wrapped(ref err) => Some(&**err)
+        match self {
+            &Codes(_) => None,
+            &Decoder(ref e) => Some(e),
+            &Http(ref e) => Some(e),
+            &Io(ref e) => Some(e)
         }
     }
 }
 
 impl FromError<DecoderError> for RecaptchaError {
     fn from_error(err: DecoderError) -> RecaptchaError {
-        RecaptchaError::Wrapped(Box::new(err))
+        RecaptchaError::Decoder(err)
     }
 }
 
 impl FromError<HttpError> for RecaptchaError {
     fn from_error(err: HttpError) -> RecaptchaError {
-        RecaptchaError::Wrapped(Box::new(err))
+        RecaptchaError::Http(err)
     }
 }
 
 impl FromError<io::Error> for RecaptchaError {
     fn from_error(err: io::Error) -> RecaptchaError {
-        RecaptchaError::Wrapped(Box::new(err))
+        RecaptchaError::Io(err)
     }
 }
