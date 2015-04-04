0df77d5804636035bfd4a67385368a0629544440
panicbit <panicbit@users.noreply.github.com>
2015-04-04 23:30:09 +0200

    Use convert::From instead error::FromError

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,8 @@
-use std::error::{Error, FromError};
+use std::error::Error;
 use std::fmt::{self, Display, Debug};
 use std::collections::HashSet;
 use std::io;
+use std::convert::From;
 use rustc_serialize::json::DecoderError;
 use hyper::HttpError;
 use super::RecaptchaErrorCode;
@@ -47,20 +48,20 @@ impl Error for RecaptchaError {
     }
 }
 
-impl FromError<DecoderError> for RecaptchaError {
-    fn from_error(err: DecoderError) -> RecaptchaError {
+impl From<DecoderError> for RecaptchaError {
+    fn from(err: DecoderError) -> RecaptchaError {
         RecaptchaError::Decoder(err)
     }
 }
 
-impl FromError<HttpError> for RecaptchaError {
-    fn from_error(err: HttpError) -> RecaptchaError {
+impl From<HttpError> for RecaptchaError {
+    fn from(err: HttpError) -> RecaptchaError {
         RecaptchaError::Http(err)
     }
 }
 
-impl FromError<io::Error> for RecaptchaError {
-    fn from_error(err: io::Error) -> RecaptchaError {
+impl From<io::Error> for RecaptchaError {
+    fn from(err: io::Error) -> RecaptchaError {
         RecaptchaError::Io(err)
     }
 }
