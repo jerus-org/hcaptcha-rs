6dfd7b0277d384426512be6a47def987563e0419
Jeremiah Russell <jrussell@jerus.ie>
2020-11-06 16:32:02 +0000

    Public methods on Recaptcha to access the fields in the RecaptchaResponse

--- src/lib.rs
+++ src/lib.rs
@@ -137,10 +137,56 @@ impl Hcaptcha {
             (false, _) => Err(Error::Codes(HashSet::new())),
         }
     }
+
+    /// Get the hostname returned in the response
+    /// Option string containig the hostname of the site
+    /// where the captcha was solved
+    ///
+    #[allow(dead_code)]
+    pub fn get_hostname(&self) -> Option<String> {
+        self.response.get_hostname()
+    }
+
+    /// Get the timestamp from the response
+    /// Option string containing the timestamp of the captcha
+    /// (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+    ///
+    #[allow(dead_code)]
+    pub fn get_timestamp(&self) -> Option<String> {
+        self.response.get_timestamp()
+    }
+
+    /// Get the credit flag
+    /// Optional flag showing whether the response will be credited
+    ///
+    #[allow(dead_code)]
+    pub fn get_credit(&self) -> Option<bool> {
+        self.response.get_credit()
+    }
+
+    /// Get the score
+    ///
+    /// ENTERPRISE feature: a score denoting malicious activity.
+    ///
+    #[allow(dead_code)]
+    pub fn get_score(&self) -> Option<f32> {
+        self.response.get_score()
+    }
+
+    /// Get the reasons for the score
+    ///
+    /// ENTERPRISE feature: reason(s) for score.
+    /// See [BotStop.com](https://BotStop.com) for details.
+    ///
+    #[allow(dead_code)]
+    pub fn get_score_reason(&self) -> Option<HashSet<String>> {
+        self.response.get_score_reason()
+    }
 }
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::json;
 
     #[tokio::test]
     async fn test_invalid_secret_missing_response() {
@@ -202,5 +248,79 @@ mod tests {
         };
 
         println!("{:?}", response);
+    }
+
+    fn test_response() -> HcaptchaResponse {
+        let response = json!({
+            "success": true,
+            "challenge_ts": "2020-11-11T23:27:00Z",
+            "hostname": "my-host.ie",
+            "credit": false,
+            "error-codes": ["missing-input-secret", "foo"],
+            "score": null,
+            "score_reason": [],
+        });
+        serde_json::from_value(response).unwrap()
+    }
+
+    #[test]
+    fn get_success_test() {
+        let response = test_response();
+
+        assert_eq!(response.get_success(), true);
+    }
+
+    #[test]
+    fn get_timestamp_test() {
+        let response = test_response();
+
+        assert_eq!(
+            response.get_timestamp(),
+            Some("2020-11-11T23:27:00Z".to_owned())
+        );
+    }
+
+    #[test]
+    fn get_hostname_test() {
+        let response = test_response();
+
+        assert_eq!(response.get_hostname(), Some("my-host.ie".to_owned()));
+    }
+
+    #[test]
+    fn get_credit_test() {
+        let response = test_response();
+
+        assert_eq!(response.get_credit(), Some(false));
+    }
+
+    #[test]
+    fn get_error_codes_test() {
+        let response = test_response();
+
+        assert!(response.get_error_codes().is_some());
+        match response.get_error_codes() {
+            Some(hash_set) => assert_eq!(hash_set.len(), 2),
+            None => {}
+        }
+    }
+
+    #[test]
+    fn get_score_test() {
+        let response = test_response();
+
+        assert!(response.get_score().is_none());
+    }
+
+    #[test]
+    fn get_score_reason_test() {
+        let response = test_response();
+        println!("The response: {:?}", response);
+
+        assert!(response.get_score_reason().is_some());
+        match response.get_score_reason() {
+            Some(hash_set) => assert!(hash_set.is_empty()),
+            None => {}
+        }
     }
 }
