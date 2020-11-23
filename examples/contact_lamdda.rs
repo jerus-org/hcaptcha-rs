9c36481fee149e069285bed9b629790f26735709
Jeremiah Russell <jrussell@jerus.ie>
2020-11-23 14:30:45 +0000

    Updated exampled based on verified contact form processor

--- examples/contact_lamdda.rs
+++ examples/contact_lamdda.rs
@@ -43,6 +43,7 @@ fn get_environment_level() -> LevelFilter {
 mod handler {
     use hcaptcha::Hcaptcha;
     use lambda_runtime::{error::HandlerError, Context};
+    use log::{debug, error};
     use rusoto_core::Region;
     use rusoto_ssm::{GetParameterRequest, Ssm, SsmClient};
     use serde_derive::{Deserialize, Serialize};
@@ -75,15 +76,10 @@ mod handler {
 
     #[derive(Deserialize, Serialize, Clone, Default)]
     pub struct ContactForm {
-        #[serde(default)]
         pub name: String,
-        #[serde(default)]
         pub phone: String,
-        #[serde(default)]
         pub email: String,
-        #[serde(default)]
         pub message: String,
-        #[serde(default)]
         #[serde(rename = "captchaResponse")]
         pub captcha_response: String,
     }
@@ -101,65 +97,94 @@ mod handler {
         Hcaptcha(#[from] hcaptcha::HcaptchaError),
     }
 
-    impl ContactForm {
-        async fn valid_captcha(&self) -> Result<(), MyError> {
-            // secret key is stored encrypted in SSM Parameter store
-            let name = "/hcaptcha/secret_key".to_owned();
-            let decrypt = true;
-            let parameter_request = GetParameterRequest {
-                name,
-                with_decryption: Some(decrypt),
-            };
-
-            let client = SsmClient::new(Region::EuWest1);
-
-            let result = client.get_parameter(parameter_request).await?;
-
-            let hcaptcha_secret;
-            match result.parameter {
-                Some(param) => match param.value {
-                    Some(value) => hcaptcha_secret = value,
-                    None => hcaptcha_secret = "".to_owned(),
-                },
-                None => hcaptcha_secret = "".to_owned(),
-            }
-
-            Hcaptcha::new(&hcaptcha_secret, &self.captcha_response)
-                .verify()
-                .await?;
-            Ok(())
-        }
-
-        async fn save(&self) {
-            todo!()
-        }
-
-        async fn send(&self) {
-            todo!()
-        }
-
-        async fn acknowledge(&self) {
-            todo!()
-        }
-
-        async fn check_errors(&self) -> Result<CustomOutput, MyError> {
-            todo!()
+    impl From<MyError> for HandlerError {
+        fn from(err: MyError) -> HandlerError {
+            HandlerError::from(err.to_string().as_str())
         }
     }
 
     #[tokio::main]
     pub async fn my_handler(e: CustomEvent, _c: Context) -> Result<CustomOutput, HandlerError> {
+        debug!("The event logged is: {:?}", e);
+
         let body_str = e.body.unwrap_or_else(|| "".to_owned());
 
-        let contact_form: ContactForm = serde_json::from_str(&body_str).unwrap_or_default();
+        let contact_form: ContactForm = serde_json::from_str(&body_str)?;
 
-        contact_form.valid_captcha().await?;
+        // Validate that the formm is submitted from a human
+        valid_captcha(&contact_form.captcha_response).await?;
 
-        let save = contact_form.save().await;
-        let send = contact_form.send().await;
-        let ack = contact_form.acknowledge().await;
+        // Create async futures work with the contact form data
+        let info_fut = info(&contact_form);
+        let notification_fut = notification(&contact_form);
+        let write_fut = write(&contact_form);
 
-        let result = contact_form.check_errors().await?;
-        Ok(result)
+        // Execute the futures concurrently
+        let (info, notification, write) = join!(info_fut, notification_fut, write_fut);
+
+        if let Err(e) = notification {
+            // Log the error and return error for rework at the client
+            error!("Notification not sent: {}", e);
+            return Err("Notifcation not sent".into());
+        }
+
+        if let Err(e) = info {
+            // Log the error and return error for rework at the client
+            error!("Info not sent to office: {}", e);
+            return Err("Info not sent to office".into());
+        }
+
+        if let Err(e) = write {
+            // Log the error but client will expect success
+            error!("Contact information not written to database: {}", e);
+        }
+
+        Ok(CustomOutput::new(
+            200,
+            format!("{}, thank you for your contact request.", contact_form.name),
+        ))
+    }
+
+    use tokio::join;
+
+    async fn valid_captcha(captcha_response: &str) -> Result<(), MyError> {
+        let name = "/hcaptcha/secret_key".to_owned();
+        let parameter_request = GetParameterRequest {
+            name,
+            with_decryption: Some(true),
+        };
+
+        let client = SsmClient::new(Region::EuWest1);
+
+        let result = client.get_parameter(parameter_request).await?;
+
+        let hcaptcha_secret;
+        match result.parameter {
+            Some(param) => match param.value {
+                Some(value) => hcaptcha_secret = value,
+                None => hcaptcha_secret = "".to_owned(),
+            },
+            None => hcaptcha_secret = "".to_owned(),
+        }
+
+        Hcaptcha::new(&hcaptcha_secret, captcha_response)
+            .verify()
+            .await?;
+        Ok(())
+    }
+
+    async fn info(contact_form: &ContactForm) -> Result<(), MyError> {
+        // Implement SES code to send email to the company info address
+        todo!()
+    }
+
+    async fn notification(contact_form: &ContactForm) -> Result<(), MyError> {
+        // Implement SES code to send email to the customer
+        todo!()
+    }
+
+    async fn write(contact_form: &ContactForm) -> Result<(), MyError> {
+        // Implement code to record contact
+        todo!()
     }
 }
