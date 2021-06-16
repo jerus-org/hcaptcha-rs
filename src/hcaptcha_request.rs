863c0d433ae1b8d1d5e1088f28ea5e3fe03fca1a
Jeremiah Russell <jrussell@jerus.ie>
2021-06-16 16:24:17 +0100

    Documentation review for HcaptchaRequest

--- src/hcaptcha_request.rs
+++ src/hcaptcha_request.rs
@@ -1,5 +1,38 @@
 //! Provides a struct to collect the required and optional parameters for
 //! the hcaptcha api request.
+//!
+//! # Example
+//!
+//! ```
+//!     use hcaptcha::HcaptchaRequest;
+//! # #[tokio::main]
+//! # async fn main() -> Result<(), hcaptcha::HcaptchaError> {
+//!     let secret = get_your_secret();         // your secret key
+//!     let bad_token = get_your_token();       // user's response token
+//!     let site_key = get_your_site_key();     // your site key
+//!     let user_ip = get_user_ip_address();    // user's ip address
+//!
+//!     let request = HcaptchaRequest::new(&secret, &bad_token)? // <- returns error
+//!         .set_site_key(site_key)
+//!         .set_user_ip(user_ip);
+//! # Ok(())
+//! # }
+//! # fn get_your_secret() -> String {
+//! #   "0x123456789abcde0f123456789abcdef012345678".to_string()
+//! # }
+//! # fn get_your_token() -> String {
+//! #    "thisisnotapropertoken".to_string()
+//! # }
+//! # use std::net::{IpAddr, Ipv4Addr};
+//! # fn get_user_ip_address() -> IpAddr {
+//! #    IpAddr::V4(Ipv4Addr::new(192, 168, 0, 17))
+//! # }
+//! # use uuid::Uuid;
+//! # fn get_your_site_key() -> Uuid {
+//! #    Uuid::new_v4()
+//! # }
+//!
+//! ```
 
 use crate::HcaptchaError;
 use std::net::IpAddr;
@@ -15,16 +48,17 @@ use hcaptcha_secret::HcaptchaSecret;
 #[allow(missing_doc_code_examples)]
 #[derive(Debug, Default, serde::Serialize)]
 pub struct HcaptchaRequest {
-    /// The response from the client's call to API
+    /// The response from the client's call to Hcaptcha client side API.
     response: HcaptchaClientResponse,
-    /// The secret_key for the site_key used by the client to call the API
+    /// The secret_key related to the site_key used to capture the response.
     secret: HcaptchaSecret,
-    /// The ip address of the client making the call (optional)
+    /// Optional: The ip address of the client providing the response.
     user_ip: Option<IpAddr>,
-    /// The site_key used by the client to make the call (optional, recommended)
+    /// Optional: The site_key used by the client to generate the response (recommended).
     site_key: Option<String>,
 }
 
+#[allow(missing_doc_code_examples)]
 impl HcaptchaRequest {
     /// Create a new HcaptchaRequest
     ///
@@ -41,16 +75,22 @@ impl HcaptchaRequest {
     ///
     /// # Example
     ///
-    /// ```no_run
-    /// use hcaptcha::HcaptchaRequest;
-    /// # main() {
-    /// let secret = get_seret_from_store();
-    /// let response = get_response_from_client();
-    ///  
-    /// let request = HcaptchaRequest::new(secret, response);
+    /// ``` no_run
+    ///     use hcaptcha::HcaptchaRequest;
+    /// # fn main() -> Result<(), hcaptcha::HcaptchaError>{
+    ///     let secret = get_your_secret();     // your secret key
+    ///     let bad_token = get_your_token();   // user's token
     ///
+    ///     let request = HcaptchaRequest::new(&secret, &bad_token)?; // <- returns error
+    /// # Ok(())
     /// # }
-    /// ```
+    /// # fn get_your_secret() -> String {
+    /// #   "0x123456789abcde0f123456789abcdef012345678".to_string()
+    /// # }
+    /// # fn get_your_token() -> String {
+    /// #    "thisisnotapropertoken".to_string()
+    /// # }
+    ///  ```
     /// # Logging
     ///
     /// If the tracing feature is enabled a debug level span is set for the
@@ -78,17 +118,35 @@ impl HcaptchaRequest {
     ///
     /// Mutate the HcaptchaRequest by adding the client Ip Address.
     ///
-    /// #Input
+    /// # Example
+    /// ``` no_run
+    ///     use hcaptcha::HcaptchaRequest;
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), hcaptcha::HcaptchaError> {
+    ///     let secret = get_your_secret();         // your secret key
+    ///     let bad_token = get_your_token();       // user's response token
+    ///     let user_ip = get_user_ip_address();    // user's ip address
     ///
-    ///     user_ip     The ip address of the client providing the response
+    ///     let request = HcaptchaRequest::new(&secret, &bad_token)? // <- returns error
+    ///         .set_user_ip(user_ip);
+    /// # Ok(())
+    /// # }
+    /// # fn get_your_secret() -> String {
+    /// #   "0x123456789abcde0f123456789abcdef012345678".to_string()
+    /// # }
+    /// # fn get_your_token() -> String {
+    /// #    "thisisnotapropertoken".to_string()
+    /// # }
+    /// # use std::net::{IpAddr, Ipv4Addr};
+    /// # fn get_user_ip_address() -> IpAddr {
+    /// #    IpAddr::V4(Ipv4Addr::new(192, 168, 0, 17))
+    /// # }
     ///
-    /// #Output
-    ///
-    ///     Updated HcaptchaRequest structure.
+    /// ```
     ///
     /// #Logging
     ///
-    /// If the tracing feature is enabled a debug level span is set for the
+    /// If the `trace` feature is enabled a debug level span is set for the
     /// method.
     /// The secret field is not logged.
     ///
@@ -102,7 +160,7 @@ impl HcaptchaRequest {
             level = "debug"
         )
     )]
-    pub fn set_user_ip(&mut self, user_ip: IpAddr) -> &HcaptchaRequest {
+    pub fn set_user_ip(mut self, user_ip: IpAddr) -> HcaptchaRequest {
         self.user_ip = Some(user_ip);
         self
     }
@@ -111,34 +169,36 @@ impl HcaptchaRequest {
     ///
     /// Mutate the HcaptchaRequest by adding the site_key.
     ///
-    /// #Input
-    ///
-    ///     site_key     The site_key used to generate the response
-    ///
-    /// #Output
-    ///
-    ///     Updated HcaptchaRequest structure.
-    ///
     /// # Example
-    /// Create a new request and set the user_ip fied in the request.
-    /// ```should_panic no_run
-    /// use hcaptcha::HcaptchaRequest;
-    /// use std::net::{IpAddr, Ipv4Addr};
+    /// Create a new request and set the site_key field in the request.
+    /// ```
+    ///     use hcaptcha::HcaptchaRequest;
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), hcaptcha::HcaptchaError> {
-    /// let secret = "0x0000000000000000000000000000000000000000"; // your secret key
-    /// let token = "this_is_likely_invalid";  // user's token
-    /// let user_ip = IpAddr::V4(Ipv4Addr::new(192, 168, 0, 17));
+    ///     let secret = get_your_secret();     // your secret key
+    ///     let bad_token = get_your_token();   // user's token
+    ///     let site_key = get_your_site_key(); // your site key
     ///
-    /// let request = HcaptchaRequest::new(secret, token)?
-    ///     .set_user_ip(&user_ip);
-    ///
+    ///     let request = HcaptchaRequest::new(&secret, &bad_token)? // <- returns error
+    ///         .set_site_key(site_key);
+    /// # Ok(())
     /// # }
+    /// # fn get_your_secret() -> String {
+    /// #   "0x123456789abcde0f123456789abcdef012345678".to_string()
+    /// # }
+    /// # fn get_your_token() -> String {
+    /// #    "thisisnotapropertoken".to_string()
+    /// # }
+    /// # use uuid::Uuid;
+    /// # fn get_your_site_key() -> Uuid {
+    /// #    Uuid::new_v4()
+    /// # }
+    ///
     /// ```
     ///
     /// #Logging
     ///
-    /// If the tracing feature is enabled a debug level span is set for the
+    /// If the `trace` feature is enabled a debug level span is created for the
     /// method.
     /// The secret field is not logged.
     ///
@@ -151,7 +211,7 @@ impl HcaptchaRequest {
             level = "debug"
         )
     )]
-    pub fn set_site_key(&mut self, site_key: Uuid) -> &HcaptchaRequest {
+    pub fn set_site_key(mut self, site_key: Uuid) -> HcaptchaRequest {
         self.site_key = Some(site_key.to_hyphenated().to_string());
         self
     }
