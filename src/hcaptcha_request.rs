833cf09daca0ea9fabd700b6aa336668046c1541
Jeremiah Russell <jrussell@jerus.ie>
2021-06-22 09:15:41 +0100

    Initial refactor of hcaptcha_request to use HcaptchaCaptcha struct

--- src/hcaptcha_request.rs
+++ src/hcaptcha_request.rs
@@ -33,30 +33,20 @@
 //!
 //! ```
 
+use crate::domain::HcaptchaSecret;
+use crate::HcaptchaCaptcha;
 use crate::HcaptchaError;
-
-mod hcaptcha_client_response;
-mod hcaptcha_secret;
-mod hcaptcha_site_key;
-mod hcaptcha_user_ip;
-
-use hcaptcha_client_response::HcaptchaClientResponse;
-use hcaptcha_secret::HcaptchaSecret;
-use hcaptcha_site_key::HcaptchaSiteKey;
-use hcaptcha_user_ip::HcaptchaUserIp;
 
 /// Type to capture the required and optional data for a call to the hcaptcha API
 #[allow(missing_doc_code_examples)]
 #[derive(Debug, Default, serde::Serialize)]
 pub struct HcaptchaRequest {
-    /// The response from the client's call to Hcaptcha client side API.
-    response: HcaptchaClientResponse,
+    /// The Hcaptcha response data returned from the client's call to
+    /// Hcaptcha client side API. May include user_ip and site_key from
+    /// the client.
+    captcha: HcaptchaCaptcha,
     /// The secret_key related to the site_key used to capture the response.
     secret: HcaptchaSecret,
-    /// Optional: The ip address of the client providing the response.
-    user_ip: Option<HcaptchaUserIp>,
-    /// Optional: The site_key used by the client to generate the response (recommended).
-    site_key: Option<HcaptchaSiteKey>,
 }
 
 #[allow(missing_doc_code_examples)]
@@ -67,7 +57,7 @@ impl HcaptchaRequest {
     ///
     /// The Hcaptcha API has two mandatory paramaters:
     ///     secret:     The client's secret key for authentication
-    ///     response:   The response code to validate
+    ///     captcha:    [HcaptchaCaptch] struct including response
     ///
     /// # Output
     ///
@@ -80,16 +70,16 @@ impl HcaptchaRequest {
     ///     use hcaptcha::HcaptchaRequest;
     /// # fn main() -> Result<(), hcaptcha::HcaptchaError>{
     ///     let secret = get_your_secret();     // your secret key
-    ///     let bad_token = get_user_token();   // user's token
+    ///     let captcha = get_captcha();        // captcha struct with user's token
     ///
-    ///     let request = HcaptchaRequest::new(&secret, &bad_token)?; // <- returns error
+    ///     let request = HcaptchaRequest::new(&secret, captcha)?;
     /// # Ok(())
     /// # }
     /// # fn get_your_secret() -> String {
     /// #   "0x123456789abcde0f123456789abcdef012345678".to_string()
     /// # }
-    /// # fn get_user_token() -> String {
-    /// #    "thisisnotapropertoken".to_string()
+    /// # fn get_user_token() -> HcaptchaCaptcha {
+    /// #    HcaptchaCaptcha::new("thisisnotapropertoken")
     /// # }
     ///  ```
     /// # Logging
@@ -102,22 +92,75 @@ impl HcaptchaRequest {
     #[cfg_attr(
         feature = "trace",
         tracing::instrument(
-            name = "Create new HcaptchaRequest with required paramaters.",
+            name = "Create new HcaptchaRequest from HcaptchaCaptcha struct.",
             skip(secret),
             level = "debug"
         )
     )]
-    pub fn new(secret: &str, response: &str) -> Result<HcaptchaRequest, HcaptchaError> {
+    pub fn new(secret: &str, captcha: HcaptchaCaptcha) -> Result<HcaptchaRequest, HcaptchaError> {
         Ok(HcaptchaRequest {
-            response: HcaptchaClientResponse::parse(response.to_owned())?,
+            captcha,
             secret: HcaptchaSecret::parse(secret.to_owned())?,
-            ..HcaptchaRequest::default()
         })
+    }
+
+    /// Create a new HcaptchaRequest from only the response string
+    ///
+    /// # Input
+    ///
+    /// The Hcaptcha API has two mandatory paramaters:
+    ///     secret:     The client's secret key for authentication
+    ///     response:    The response code to validate
+    ///
+    /// # Output
+    ///
+    /// A HcaptchaRequest struct is returned if the inputs are valid.
+    /// A HcaptchaError is returned if the validation fails.
+    ///
+    /// # Example
+    ///
+    /// ``` no_run
+    ///     use hcaptcha::HcaptchaRequest;
+    /// # fn main() -> Result<(), hcaptcha::HcaptchaError>{
+    ///     let secret = get_your_secret();     // your secret key
+    ///     let response = get_user_token();    // Hcaptcha client response
+    ///
+    ///     let request = HcaptchaRequest::new(&secret, &response)?;
+    /// # Ok(())
+    /// # }
+    /// # fn get_your_secret() -> String {
+    /// #   "0x123456789abcde0f123456789abcdef012345678".to_string()
+    /// # }
+    /// # fn get_user_token() -> String {
+    /// #    "response_string".to_owend()
+    /// # }
+    ///  ```
+    /// # Logging
+    ///
+    /// If the tracing feature is enabled a debug level span is set for the
+    /// method.
+    /// The secret field will not be logged.
+    ///
+    #[allow(dead_code)]
+    #[cfg_attr(
+        feature = "trace",
+        tracing::instrument(
+            name = "Create new HcaptchaRequest from response string.",
+            skip(secret),
+            level = "debug"
+        )
+    )]
+    pub fn new_from_response(
+        secret: &str,
+        response: &str,
+    ) -> Result<HcaptchaRequest, HcaptchaError> {
+        let captcha = HcaptchaCaptcha::new(response)?;
+        Ok(HcaptchaRequest::new(secret, captcha)?)
     }
 
     /// Specify the optional ip address value
     ///
-    /// Mutate the HcaptchaRequest by adding the client Ip Address.
+    /// Update client IP adress.
     ///
     /// # Example
     /// ``` no_run
@@ -125,10 +168,10 @@ impl HcaptchaRequest {
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), hcaptcha::HcaptchaError> {
     ///     let secret = get_your_secret();         // your secret key
-    ///     let bad_token = get_user_token();       // user's response token
+    ///     let response  = get_response();       // user's response token
     ///     let user_ip = get_user_ip_address();    // user's ip address
     ///
-    ///     let request = HcaptchaRequest::new(&secret, &bad_token)? // <- returns error
+    ///     let request = HcaptchaRequest::new_from_response(&secret, &response)?
     ///         .set_user_ip(&user_ip)?;
     /// # Ok(())
     /// # }
@@ -157,18 +200,32 @@ impl HcaptchaRequest {
         tracing::instrument(
             name = "Request verification from hcaptcha.",
             skip(self),
-            fields(response = %self.response),
+            fields(captcha = ?self.captcha),
             level = "debug"
         )
     )]
-    pub fn set_user_ip(mut self, user_ip: &str) -> Result<Self, HcaptchaError> {
-        self.user_ip = Some(HcaptchaUserIp::parse(user_ip.to_owned())?);
-        Ok(self)
+    pub fn set_user_ip(self, user_ip: &str) -> Result<Self, HcaptchaError> {
+        let get_string = |x| -> &str {
+            if let Some(y) = x {
+                y.as_str()
+            } else {
+                ""
+            }
+        };
+
+        let site_key = get_string(&self.captcha.site_key());
+        let user_ip = get_string(&self.captcha.user_ip());
+
+        let captcha = HcaptchaCaptcha::new(&self.captcha.response().as_str()).set_site_key;
+        let request = HcaptchaRequest::new(&self.secret().as_str(), captcha);
+
+        &self.captcha.set_user_ip(user_ip)?;
+        Ok(request)
     }
 
     /// Specify the optional site_key value
     ///
-    /// Mutate the HcaptchaRequest by adding the site_key.
+    /// Update the site_key.
     ///
     /// # Example
     /// Create a new request and set the site_key field in the request.
@@ -177,18 +234,18 @@ impl HcaptchaRequest {
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), hcaptcha::HcaptchaError> {
     ///     let secret = get_your_secret();     // your secret key
-    ///     let bad_token = get_user_token();   // user's token
+    ///     let captcha = get_captcha();        // captcha
     ///     let site_key = get_your_site_key(); // your site key
     ///
-    ///     let request = HcaptchaRequest::new(&secret, &bad_token)? // <- returns error
+    ///     let request = HcaptchaRequest::new(&secret, captcha)?
     ///         .set_site_key(&site_key);
     /// # Ok(())
     /// # }
     /// # fn get_your_secret() -> String {
     /// #   "0x123456789abcde0f123456789abcdef012345678".to_string()
     /// # }
-    /// # fn get_user_token() -> String {
-    /// #    "thisisnotapropertoken".to_string()
+    /// # fn get_captcha() -> HcaptchaCaptcha {
+    /// #    HcaptchaCaptcha::new("thisisnotapropertoken")
     /// # }
     /// # use uuid::Uuid;
     /// # fn get_your_site_key() -> String {
@@ -208,19 +265,78 @@ impl HcaptchaRequest {
         tracing::instrument(
             name = "Request verification from hcaptcha.",
             skip(self),
-            fields(response = %self.response),
+            fields(captcha = ?self.captcha),
             level = "debug"
         )
     )]
     pub fn set_site_key(mut self, site_key: &str) -> Result<Self, HcaptchaError> {
-        self.site_key = Some(HcaptchaSiteKey::parse(site_key.to_owned())?);
+        self.captcha.set_site_key(site_key)?;
         Ok(self)
+    }
+
+    pub(crate) fn secret(&self) -> HcaptchaSecret {
+        self.secret
     }
 }
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
     // use crate::error::Code::*;
     // use crate::HcaptchaError::*;
     // use std::collections::HashSet;
+    use crate::HcaptchaCaptcha;
+    use claim::{assert_none, assert_ok};
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
+    use std::iter;
+
+    fn random_hex_string(len: usize) -> String {
+        let mut rng = thread_rng();
+        let chars: String = iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric))
+            .map(char::from)
+            .take(len / 2)
+            .collect();
+
+        hex::encode(chars)
+    }
+
+    fn random_response() -> String {
+        let mut rng = thread_rng();
+        iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric))
+            .map(char::from)
+            .take(100)
+            .collect()
+    }
+
+    fn dummy_captcha() -> HcaptchaCaptcha {
+        HcaptchaCaptcha::new(&random_response())
+            .unwrap()
+            .set_user_ip(&fakeit::internet::ipv4_address())
+            .unwrap()
+            .set_site_key(&fakeit::unique::uuid_v4())
+            .unwrap()
+    }
+
+    #[test]
+    fn valid_new_from_captcha_struct() {
+        let secret = format!("0x{}", random_hex_string(40));
+        let captcha = dummy_captcha();
+
+        assert_ok!(HcaptchaRequest::new(&secret, captcha));
+    }
+
+    #[test]
+    fn valid_new_from_response() {
+        let secret = format!("0x{}", random_hex_string(40));
+        let response = random_response();
+
+        let request = HcaptchaRequest::new_from_response(&secret, &response).unwrap();
+
+        assert_eq!(&secret, &request.secret().as_str());
+        assert_eq!(&response, &request.captcha.response().as_str());
+        assert_none!(&request.captcha.user_ip());
+        assert_none!(&request.captcha.site_key());
+    }
 }
