9fc194dd46899d7927228656d1a579c61cb9c4b9
Jeremiah Russell <jrussell@jerus.ie>
2021-10-07 11:52:51 +0100

    Refactor tests and tests for invalid inputs for the verification data

--- test_suite/src/lib.rs
+++ test_suite/src/lib.rs
@@ -1,1 +1,8 @@
-
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
---
+++ test_suite/tests/ui/pass/test_invalid_token.rs
@@ -0,0 +1,66 @@
+mod helper;
+
+use hcaptcha::{Hcaptcha, Code};
+use serde_json::json;
+use wiremock::matchers::{body_string, method, path};
+use wiremock::{Mock, MockServer, ResponseTemplate};
+use chrono::{Duration, Utc};
+
+#[derive(Debug, Hcaptcha)]
+struct Test {
+    #[captcha]
+    hcaptcha: String,
+    #[sitekey]
+    sitekey: String,
+    #[remoteip]
+    ip: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // Setup
+    let token = "    ".to_string(); // String containing only spaces
+    // let token = helper::random_string(100);
+    let remoteip = fakeit::internet::ipv4_address();
+    let sitekey = fakeit::unique::uuid_v4();
+    let secret = format!("0x{}", hex::encode(helper::random_string(20)));
+
+    let expected_body = format!("response={}&remoteip={}&sitekey={}&secret={}", &token, &remoteip, &sitekey, &secret);
+
+    let timestamp = Utc::now()
+        .checked_sub_signed(Duration::minutes(10))
+        .unwrap()
+        .to_rfc3339();
+
+    let response_template = ResponseTemplate::new(200).set_body_json(json!({
+        "success": true,
+        "challenge_ts": timestamp,
+        "hostname": "test-host",
+    }));
+
+    let mock_server = MockServer::start().await;
+    Mock::given(method("POST"))
+        .and(path("/siteverify"))
+        .and(body_string(&expected_body))
+        .respond_with(response_template)
+        .mount(&mock_server)
+        .await;
+
+    let uri = format!("{}{}", mock_server.uri(), "/siteverify");
+
+    let form = Test { hcaptcha: token, sitekey, ip: remoteip };
+    let response = form.valid_response(&secret, Some(uri)).await;
+
+    
+    claim::assert_err!(&response);
+    
+    if let Err(codes) = response {
+        match codes {
+            hcaptcha::HcaptchaError::Codes(hash_set) => {
+                assert_eq!(hash_set.len(), 1);
+                assert!(hash_set.contains(&Code::MissingResponse));
+            }
+            _ => unreachable!(),
+        }
+    };
+}
