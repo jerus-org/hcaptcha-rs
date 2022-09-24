7e155cc963792abef25bfe0ab234547767cc1b98
Jeremiah Russell <jerry@jrussell.ie>
2022-09-24 13:56:40 +0100

    Update docs warning feature flag

--- hcaptcha_derive/src/lib.rs
+++ hcaptcha_derive/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(missing_docs)]
-#![cfg_attr(docsrs, warn(rustdoc::missing_doc_code_examples))]
+#![cfg_attr(docsrs, feature(rustdoc_missing_doc_code_examples))]
 #![cfg_attr(docsrs, warn(rustdoc::invalid_codeblock_attributes))]
 
 //! Derive macro for hcaptcha::Hcaptcha
