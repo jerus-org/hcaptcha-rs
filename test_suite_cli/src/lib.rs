887fe772dd719790a59d20e4308570d2a6c94ffb
Jeremiah Russell <jerry@jrussell.ie>
2024-09-19 16:06:19 +0100

    refactor: clean up commented-out code and improve output formatting in test_suite_cli

--- test_suite_cli/src/lib.rs
+++ test_suite_cli/src/lib.rs
@@ -3,12 +3,8 @@ use std::{path::PathBuf, process::Command};
 
 #[cfg(target_os = "linux")]
 pub fn cargo_bin(name: &str) -> std::path::PathBuf {
-    // let file_name = format!("{}{}", name, std::env::consts::EXE_SUFFIX);
-    // let target_dir = target_dir();
-    // target_dir.join(file_name)
     let bin_dir = bin_dir();
     bin_dir.join(name)
-    // target_dir.join(name)
 }
 
 #[cfg(target_os = "wasi")]
@@ -25,34 +21,19 @@ fn bin_dir() -> PathBuf {
     PathBuf::new().join("bin")
 }
 
-// Adapted from
-// https://github.com/rust-lang/cargo/blob/485670b3983b52289a2f353d589c57fae2f60f82/tests/testsuite/support/mod.rs#L507
-// fn target_dir() -> std::path::PathBuf {
-//     std::env::current_exe()
-//         .ok()
-//         .map(|mut path| {
-//             path.pop();
-//             if path.ends_with("deps") {
-//                 path.pop();
-//             }
-//             path
-//         })
-//         .unwrap()
-// }
-
 pub fn assert_output(output: std::process::Output, expected: &str) {
-    eprintln!("output:{:?}", &output);
+    // eprintln!("output:{:#?}", &output);
 
     if !output.stdout.is_empty() {
         let o = String::from_utf8_lossy(&output.stdout);
-        eprintln!("output:{:?}", o);
+        eprintln!("{o}");
 
         assert_eq!(o, expected);
     };
 
     if !output.stderr.is_empty() {
         let e = String::from_utf8_lossy(&output.stderr);
-        eprintln!("error:{:?}", e);
+        eprintln!("{e}");
 
         assert_eq!(e, expected);
     };
