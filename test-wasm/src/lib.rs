30cb39f1ce7ba894c359ffa91a498abd3d267d2e
Jeremiah Russell <jerry@jrussell.ie>
2024-10-15 06:09:30 +0100

    feat(test-wasm): add hcaptcha integration and async test support

--- test-wasm/src/lib.rs
+++ test-wasm/src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use claims::assert_ok;
+use hcaptcha::Hcaptcha;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,4 +12,26 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, test-wasm!");
+}
+
+#[derive(Hcaptcha)]
+struct Test {
+    #[captcha]
+    hcaptcha: String,
+}
+
+#[wasm_bindgen]
+pub async fn valid_integration_test() {
+    let response = "10000000-aaaa-bbbb-cccc-000000000001";
+    let secret = "0x0000000000000000000000000000000000000000";
+    // let sitekey = "10000000-ffff-ffff-ffff-000000000001";
+
+    let form = Test {
+        hcaptcha: response.to_string(),
+    };
+    let response = form.valid_response(secret, None).await;
+
+    assert_ok!(&response);
+    let response = response.unwrap();
+    assert!(&response.success());
 }
--- test-wasm/tests/web.rs
+++ test-wasm/tests/web.rs
@@ -3,6 +3,7 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use test_wasm::valid_integration_test;
 use wasm_bindgen_test::*;
 
 // wasm_bindgen_test_configure!(run_in_browser);
@@ -10,4 +11,9 @@ use wasm_bindgen_test::*;
 #[wasm_bindgen_test]
 fn pass() {
     assert_eq!(1 + 1, 2);
+}
+
+#[wasm_bindgen_test]
+async fn valid_integration_test() {
+    valid_integration_test().await;
 }
