ff645639b4811271bc346593bcd893abb3665989
Jeremiah Russell <jerry@jrussell.ie>
2025-09-28 09:46:55 +0100

    📦 build(hcaptcha_derive): add release hook script
    - create release-hook.sh for building README and changelog
    - concatenate readme sections into README.md
    - generate changelog with versioning support

---
+++ hcaptcha_derive/release-hook.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Build an updated README
+cat docs/readme/head.md > README.md
+# shellcheck disable=SC2129
+cat docs/readme/lib.md >> README.md
+cat docs/main.md >> README.md
+cat docs/readme/tail.md >> README.md   
+
+# Build Changelog
+gen-changelog generate --display-summaries --name CHANGE.md --package hcaptcha_derive --next-version "$SEMVER"
