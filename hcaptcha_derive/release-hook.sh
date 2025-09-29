9d209025ae8bede1e75720434c3df1e21a9b2a1f
Jeremiah Russell <jerry@jrussell.ie>
2025-09-29 14:14:43 +0100

    🔧 chore(release-hook): enhance release script
    - switch shebang to bash for extended functionality
    - add set -exo pipefail for better error handling
    - define variables for changelog generation parameters

--- hcaptcha_derive/release-hook.sh
+++ hcaptcha_derive/release-hook.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+set -exo pipefail
+
+NAME="CHANGE.md"
+PACKAGE=hcaptcha_derive
+REPO_DIR="../."
 
 # Build Changelog
-gen-changelog generate --display-summaries --name CHANGE.md --package hcaptcha_derive --next-version "$SEMVER"
+gen-changelog generate \
+    --display-summaries \
+    --name ${NAME} \
+    --package ${PACKAGE} \
+    --repository-dir ${REPO_DIR} \
+    --next-version "$SEMVER"
